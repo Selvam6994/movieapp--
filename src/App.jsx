168f3c788da68e35fdafafb7d803adb1172f8967
Selvam6994 <selvamyoor@gmail.com>
2023-01-21 20:56:05 +0530

    Movie App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Moviecards from "./Movielist";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <Moviecards></Moviecards>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <span className="title">Movie Time</span>
+      <input className="input-feild" placeholder="Search" />
+    </div>
+  );
+}
+
+export default Nav;
